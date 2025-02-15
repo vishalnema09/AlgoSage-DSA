b904dff874c21e2a7ff04088365546cf673fe34b
Vishal Nema <vishalnema23@gmail.com>
2025-02-15 17:49:13 +0530

    169. Majority Element (Moore's Voting Algorithm)

---
+++ src/Arrays/majorityElement.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class majorityElement {
+    public int majorityElementa(int[] nums) {
+        int candidate = 0, count = 0;
+        for (int num : nums) {
+            if (count == 0) candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
