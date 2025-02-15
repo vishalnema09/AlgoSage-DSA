21e2061234f119001f1dc16b207dea8bd043debd
Vishal Nema <vishalnema23@gmail.com>
2025-02-15 18:04:24 +0530

    121. Best Time to Buy and Sell Stock

---
+++ src/Arrays/bestTimetoBuyAndSellStock.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class bestTimetoBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE, maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+}
