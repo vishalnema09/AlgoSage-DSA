87626c736a6c37f1b2e8defa00aaf4be04f9e21b
Vishal Nema <vishalnema23@gmail.com>
2025-02-16 18:07:19 +0530

    gfg Second Largest

---
+++ src/Arrays/secondLargest.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class secondLargest {
+    public int getSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int num:arr){
+            if(num>largest){
+                secondLargest = largest;
+                largest = num;
+            }
+            else if(num>secondLargest && num<largest){
+                secondLargest = num;
+            }
+        }
+        return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+    }
+}
