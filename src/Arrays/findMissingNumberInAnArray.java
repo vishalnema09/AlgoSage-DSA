e9aa6585e7dd3356e5f0126c82d7c63ecb59c653
Vishal Nema <vishalnema23@gmail.com>
2025-02-17 20:05:14 +0530

    268. Missing Number

---
+++ src/Arrays/findMissingNumberInAnArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class findMissingNumberInAnArray {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans=ans^i;
+        }
+        for(int i=0;i<n;i++){
+            ans=ans^nums[i];
+        }
+        return ans;
+    }
+}
