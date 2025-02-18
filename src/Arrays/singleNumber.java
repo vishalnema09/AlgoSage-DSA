a43702134db1a51f92e00a31c7a80fce9160b3a7
Vishal Nema <vishalnema23@gmail.com>
2025-02-18 21:08:52 +0530

    136. Single Number

---
+++ src/Arrays/singleNumber.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class singleNumber {
+    public int singlNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
