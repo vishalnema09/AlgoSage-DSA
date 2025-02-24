57ba08c2b67d9aef4020ce208fc6a7c561b2307f
Vishal Nema <vishalnema23@gmail.com>
2025-02-25 01:07:41 +0530

    leetcode 48. Rotate Image

---
+++ src/Arrays/RotateImage.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i]);
+        }
+    }
+    private void reverse(int[] row) {
+        int start = 0, end = row.length - 1;
+        while (start < end) {
+            int temp = row[start];
+            row[start] = row[end];
+            row[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
