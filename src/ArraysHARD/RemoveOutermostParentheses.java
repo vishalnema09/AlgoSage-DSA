85f1f8693d7bf7f4dc1a0dfd6d473ff59d58a853
Vishal Nema <vishalnema23@gmail.com>
2025-02-25 19:38:30 +0530

    leetcode 1021. Remove Outermost Parentheses

---
+++ src/ArraysHARD/RemoveOutermostParentheses.java
@@ -0,0 +1,19 @@
+package ArraysHARD;
+
+public class RemoveOutermostParentheses {
+    public String removeOuterParentheses(String s) {
+        StringBuilder temp = new StringBuilder();
+        StringBuilder res = new StringBuilder();
+        int count = 0;
+        for(char x:s.toCharArray()){
+            temp.append(x);
+            if(x=='(')count++;
+            else count--;
+            if(count==0){
+                res.append(temp.substring(1,temp.length()-1));
+                temp.setLength(0);
+            }
+        }
+        return res.toString();
+    }
+}
