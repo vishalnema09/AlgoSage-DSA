943c12adf8d7beaa1ea8be34459555f83c29768c
Vishal Nema <vishalnema23@gmail.com>
2025-02-27 22:34:45 +0530

    leetcode 451. Sort Characters By Frequency

---
+++ src/String/SortCharactersByFrequency.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class SortCharactersByFrequency {
+    public String frequencySort(String s) {
+        Map<Character, Integer> hm = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
+                (a, b) -> b.getValue() - a.getValue()
+        );
+
+        pq.addAll(hm.entrySet());
+
+        StringBuilder result = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry<Character, Integer> entry = pq.poll();
+            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        }
+
+        return result.toString();
+    }
+}
