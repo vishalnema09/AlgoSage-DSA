f8940f410c2edb280aafce3d9f292bb535d9ea6d
Vishal Nema <vishalnema23@gmail.com>
2025-03-01 23:29:53 +0530

    GFG SelectionSort

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i; // Assume current index is min
+
+            // Find the minimum element in the remaining unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j; // Update minIndex if smaller element found
+                }
+            }
+
+            // Swap only if minIndex changed
+            if (minIndex != i) {
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
