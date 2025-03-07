5e49f5eabff1cbef1ea5cfb782c583960483f23f
Vishal Nema <vishalnema23@gmail.com>
2025-03-07 23:15:51 +0530

    1539. Kth Missing Positive Number

---
+++ src/BinarySearch/KthMissingPositiveNumber.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class KthMissingPositiveNumber {
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low + k;
+    }
+}
