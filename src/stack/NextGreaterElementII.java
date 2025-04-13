0e6ac9be23734b74c5e2e794b0c409ebf4c6d815
Vishal Nema <vishalnema23@gmail.com>
2025-04-13 11:14:11 +0530

    leetcode 503. Next Greater Element II

---
+++ src/stack/NextGreaterElementII.java
@@ -0,0 +1,24 @@
+package stack;
+
+import java.util.Stack;
+
+public class NextGreaterElementII {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int ans[] = new int[n];
+
+        for(int i = 2*n-1; i >=0; i--){
+
+            while(!st.isEmpty() && nums[i % n] >= st.peek()){
+                st.pop();
+            }
+            if(i < n){
+                if(st.isEmpty())ans[i] = -1;
+                else ans[i] = st.peek();
+            }
+            st.push(nums[i % n]);
+        }
+        return ans;
+    }
+}
