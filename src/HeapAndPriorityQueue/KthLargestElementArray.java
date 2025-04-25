a37942285d8b469ffc3c6da01d58b9dfb6bb4417
Vishal Nema <vishalnema23@gmail.com>
2025-04-25 19:05:05 +0530

    leetcode 215. Kth Largest Element in an Array

---
+++ src/HeapAndPriorityQueue/KthLargestElementArray.java
@@ -0,0 +1,18 @@
+package HeapAndPriorityQueue;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElementArray {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
+    }
+}
