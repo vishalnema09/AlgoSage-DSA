a6f38a70f30d5f61d82059aaa4871dcd9cb80a31
Vishal Nema <vishalnema23@gmail.com>
2025-05-02 00:09:20 +0530

    leetcode 145 Iterative Postorder Using Two Stacks

---
+++ src/BinaryTree/IterativePostorderUsingTwoStacks.java
@@ -0,0 +1,31 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class IterativePostorderUsingTwoStacks {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        stack1.push(root);
+
+        while (!stack1.isEmpty()) {
+            TreeNode node = stack1.pop();
+            stack2.push(node);
+
+            if (node.left != null) stack1.push(node.left);
+            if (node.right != null) stack1.push(node.right);
+        }
+
+        while (!stack2.isEmpty()) {
+            result.add(stack2.pop().val);
+        }
+
+        return result;
+    }
+}
