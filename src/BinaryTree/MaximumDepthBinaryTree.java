40ef3e70cd7941ea8674e026dc18a9769409cbd7
Vishal Nema <vishalnema23@gmail.com>
2025-05-02 00:22:31 +0530

    leetcode 104. Maximum Depth of Binary Tree

---
+++ src/BinaryTree/MaximumDepthBinaryTree.java
@@ -0,0 +1,10 @@
+package BinaryTree;
+
+public class MaximumDepthBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if(root==null)return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth,rightDepth)+1;
+    }
+}
