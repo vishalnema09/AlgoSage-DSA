a2c28a71017921d10b4534618cbb1e2a2bdf1801
Vishal Nema <vishalnema23@gmail.com>
2025-05-15 22:51:02 +0530

    Leetcode 701. Insert into a Binary Search Tree

---
+++ src/BinarySearchTrees/InsertIntoBinarySearchTree.java
@@ -0,0 +1,17 @@
+package BinarySearchTrees;
+
+public class InsertIntoBinarySearchTree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+}
