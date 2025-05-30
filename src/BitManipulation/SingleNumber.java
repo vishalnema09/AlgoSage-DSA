478437d4b1739f3f01e58e40cf01c3b52b87f22d
Vishal Nema <vishalnema23@gmail.com>
2025-05-31 00:20:27 +0530

    Leetcode 136. Single Number

---
+++ src/BitManipulation/SingleNumber.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int num : nums) {
+            xor ^= num;
+        }
+        return xor;
+    }
+}
