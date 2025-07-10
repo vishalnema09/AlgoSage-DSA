39eaef5474f3996fc11895a2a96ed980577d2e90
Vishal Nema <vishalnema23@gmail.com>
2025-07-10 11:31:43 +0530

    Leetcode 198. House Robber as similar to the MaxSumWithoutAdjacent

---
+++ src/DynamicProgramming/HouseRobberSpaceOptimized.java
@@ -0,0 +1,19 @@
+package DynamicProgramming;
+
+public class HouseRobberSpaceOptimized {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int prev = nums[0];
+        int prev2 = 0;
+
+        for(int i = 1; i < n; i++) {
+            int take = nums[i];
+            if(i > 1) take += prev2;
+            int notTake = 0 + prev;
+            int curr = Math.max(take, notTake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
