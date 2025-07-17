5685ad434287ffa2e590738e2fe29633f33ae6cd
Vishal Nema <vishalnema23@gmail.com>
2025-07-17 23:06:22 +0530

    Leetcode 122. Best Time to Buy and Sell Stock II SPACE OPTIMIZED

---
+++ src/DynamicProgramming/OnStocks/BestTimeBuySellStockII.java
@@ -0,0 +1,18 @@
+package DynamicProgramming.OnStocks;
+
+public class BestTimeBuySellStockII {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[] ahead = new int[2];
+        int[] curr = new int[2];
+
+        for (int i = n - 1; i >= 0; i--) {
+            curr[1] = Math.max(-prices[i] + ahead[0], ahead[1]); // buy
+            curr[0] = Math.max(prices[i] + ahead[1], ahead[0]);  // sell
+
+            ahead = curr.clone();
+        }
+
+        return ahead[1];
+    }
+}
