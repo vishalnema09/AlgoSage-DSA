5f60e7c7b1e81bd3a1538cbcffb02d8a596f78b7
Vishal Nema <vishalnema23@gmail.com>
2025-07-18 11:03:14 +0530

    Leetcode 300. Longest Increasing Subsequence TAB

---
+++ src/DynamicProgramming/OnLIS/LongestIncreasingSubsequenceTab.java
@@ -0,0 +1,21 @@
+package DynamicProgramming.OnLIS;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequenceTab {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int maxLen = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+}
