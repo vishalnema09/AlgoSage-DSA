ede91a08bc082bf36ff81cd126bd182eca833b9b
Vishal Nema <vishalnema23@gmail.com>
2025-07-21 13:28:37 +0530

    Leetcode 1043. Partition Array for Maximum Sum Tab

---
+++ src/DynamicProgramming/OnSquares/PartitionArrayMaximumSumTab.java
@@ -0,0 +1,17 @@
+package DynamicProgramming.OnSquares;
+
+public class PartitionArrayMaximumSumTab {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = 1; j <= k && i - j >= 0; j++) {
+                max = Math.max(max, arr[i - j]);
+                dp[i] = Math.max(dp[i], dp[i - j] + max * j);
+            }
+        }
+        return dp[n];
+    }
+}
